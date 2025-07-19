615939e0066c94a72db5354a00bec2771346b117
Clément Barbisan <c.barbisan@free.fr>
2025-07-19 12:35:39 +0200

    add npc navmesh

--- Crunch/Assets/Scripts/NPC.cs
+++ Crunch/Assets/Scripts/NPC.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC : MonoBehaviour
 {
     [SerializeField] private AStateNPC _overworkedState;
     [SerializeField] private AStateNPC _workingState;
     [SerializeField] private AStateNPC _underworkedState;
+    public Station currentStation;
+    public NavMeshAgent agent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
