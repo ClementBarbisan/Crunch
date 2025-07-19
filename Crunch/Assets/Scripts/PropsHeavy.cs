672b2b32e4090978ada2628321be4a53453761a0
OwenDuffourg <48203107+OwenDuffourg@users.noreply.github.com>
2025-07-19 18:49:51 +0200

    Update Scale Player

---
+++ Crunch/Assets/Scripts/PropsHeavy.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class PropsHeavy : MonoBehaviour, IInteractable
+{
+    public bool Heavy => true;
+    public void Interact()
+    {
+        // Nothing
+    }
+
+    public void OnScream()
+    {
+        // Nothing
+    }
+}
