bf488fb8357c247478f1683271133a48f76ed945
OwenDuffourg <48203107+OwenDuffourg@users.noreply.github.com>
2025-07-20 13:48:10 +0200

    Prefabs Can Snack Vending Machine

--- Crunch/Assets/Scripts/PropsBoost.cs
+++ Crunch/Assets/Scripts/PropsBoost.cs
@@ -1,10 +1,12 @@
 
+using System;
 using UnityEngine;
 
 public class PropsBoost : MonoBehaviour, IInteractable
 {
-    [SerializeField] private ParticleSystem breakFx;
-    [SerializeField] private AudioClip clipBreak;
+    [SerializeField] private ParticleSystem breakFx, boostFx;
+    [SerializeField] private AudioClip clipBreak, clipBoost;
+    [SerializeField] private float boostEffectValue;
     [HideInInspector]
     public bool waitColliding;
     public bool Heavy => false;
@@ -29,18 +31,30 @@ public class PropsBoost : MonoBehaviour, IInteractable
         if (!waitColliding)
             return;
 
-        if (!collision.transform.CompareTag("Player"))
+        if (collision.transform.CompareTag("NPC"))
+        {
+            Boost();
+            if (collision.collider.TryGetComponent(out NPC npcOther))
+                npcOther.WorkStress = boostEffectValue;
+        }
+        else if (!collision.transform.CompareTag("Player"))
         {
             Breaks();
         }
     }
 
+    private void Boost()
+    {
+        waitColliding = false;
+        AudioSource.PlayClipAtPoint(clipBoost, transform.position);
+        Instantiate(boostFx, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
     private void Breaks()
     {
-        waitColliding = true;
+        waitColliding = false;
         AudioSource.PlayClipAtPoint(clipBreak, transform.position);
         Instantiate(breakFx, transform.position, Quaternion.identity);
-        GetComponent<MeshRenderer>().enabled = false;
         Destroy(gameObject);
     }
 }
